8a8806cc9daf8f5922489b44a8f0219e676b45a5
skookumer <ruminator@DESKTOP-41LQTGQ.localdomain>
2026-06-10 16:39:23 -0400

    updated method

--- data_loader.py
+++ data_loader.py
@@ -10,21 +10,27 @@ import openslide
 from PIL import Image
 import torch
 from torchstain import normalizers
+from torchvision import transforms
 import os
 
 from collections import defaultdict
 from pprint import pprint
+from tqdm import tqdm
+
+from joblib import Parallel, delayed
 
 DATA_PATH = Path(r"C:\Users\Eric Arnold\Documents\TCGA_data\tcga_brca_slides")
-LOG_PATH = Path(__file__).parent
+LOG_PATH = Path(__file__).parent / "logs"
 LOG_PATH.mkdir(exist_ok=True)
 
+DF_PATH = Path(__file__).parent / "tile_lookup.parquet"
 REFERENCE_MPP = .25
-TILE_SIZE = 256
+TILE_SIZE = 512
+ACCOUNT_FOR_MPP = False
 
 class WSI_loader:
 
-    def __init__(self, run_name, reference_tile: tuple = (0, 10000)):
+    def __init__(self, run_name):
 
         log_path = LOG_PATH / f"{run_name}.jsonl"
 
@@ -34,93 +40,103 @@ class WSI_loader:
             self.log = pl.DataFrame(lines)
         else:
             self.log = pl.DataFrame()
-    
+        
         self.enumerate_tiles()
-        self.reference = self.get_tile(reference_tile)
-        # WSI_normalizer()
-
-    def check_finished(self):
-        last_entry = self.log[-1]
-        self.epoch = last_entry["epoch"]
-        completed_tiles = set(self.log.filter(pl.col("epoch") == self.epoch)["tile_id"])
-
-
-        if len(num_tiles) != len(completed_tiles):
-            self.patient = last_patient
-            self.to_complete = set(image_files) - set(completed_images)
-        else:
-            completed_patients = set(self.log["patient"].unique())
-            all_patients = set(DATA_PATH.iterdir())
-            candidates = all_patients - completed_patients
-
-            if len(candidates) > 0:
-                candidate = np.random.choice(candidates)
-            else:
-                self.epoch += 1
-                candidate = np.random.choice(all_patients)
-
-            self.patient = candidate
-            candidate_path = DATA_PATH / candidate
-            image_path = next(candidate_path.iterdir())
-            self.to_complete = set(image_path.iterdir())
+        self.patient_lookup = {p.name: p for p in list(DATA_PATH.iterdir())}
+        self.normalizer = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], 
+                                std=[0.229, 0.224, 0.225])
+        ])
+        self.valid_indices = self.df.filter(~pl.col("is_empty"))
 
     def enumerate_tiles(self):
-        patients = list(DATA_PATH.iterdir())
-        tile_lookup = defaultdict(dict)
-        current_tile = 0
-        for p in enumerate(patients):
+        cols = {
+            "patient_name": pl.Utf8,
+            "crd": pl.List(pl.Int64),
+            "is_empty": pl.Boolean
+        }
+
+        self.df = pl.read_parquet(DF_PATH) if DF_PATH.exists() else pl.DataFrame(schema=cols)
+        processed = set(self.df["patient_name"].unique().to_list())
+        patients = [p for p in list(DATA_PATH.iterdir()) if p.name not in processed]
+
+        def process_patient(p):
+            print(f"enumerating {p.name}")
             img = WSISlide(p)
             res = img.dim
             max_x = res[0]
             max_y = res[1]
-            tiles_per_svs = int(max_x * max_y / TILE_SIZE ** 2)
-            y_level = -1
-            for t in range(tiles_per_svs):
-                x_crd = (current_tile * TILE_SIZE) % max_x
-                if x_crd == 0:
+            tiles_per_svs = int(max_x * max_y / img.read_size ** 2)
+
+            x_old = 0
+            y_level = 0
+            current_tile = 0
+
+            rows = []
+
+            for t in tqdm(range(tiles_per_svs)):
+                row = {"patient_name": p.name, "crd": None, "is_empty": False}
+
+                x_crd = (current_tile * img.read_size) % max_x
+                if x_crd < x_old:
                     y_level += 1
-                y_crd = y_level * TILE_SIZE
-                tile_lookup[p.name][current_tile] = (x_crd, y_crd)
+                y_crd = y_level * img.read_size
+                
+                row["crd"] = (x_crd, y_crd)
+                tile = img.read_raw(x_crd, y_crd)
+                arr = np.array(tile)
+                gray = 0.2989 * arr[:,:,0] + 0.5870 * arr[:,:,1] + 0.1140 * arr[:,:,2]
+                row["is_empty"] = bool((gray > 220).mean() > 0.8)
+
                 current_tile += 1
-            y_level = -1
+                x_old = x_crd
+                rows.append(row)
 
-        self.patients = patients
-        self.tile_lookup = tile_lookup
-        self.indptr = np.cumsum([len(tile_lookup[key]) for key in tile_lookup])
-    
+            y_level = 0
+            current_tile = 0
+            return rows
+        
+        if len(patients) > 0:
+            results = Parallel(n_jobs=12)(delayed(process_patient)(p) for p in self.patients)
+            all_rows = [row for patient_rows in results for row in patient_rows]
+            df_new = pl.DataFrame(all_rows)
+            self.df = pl.concat([self.df, df_new])
+            self.df.write_parquet(DF_PATH)
 
-    def fit_normalizer(self, tile):
-        reference_tensor = torch.from_numpy(np.array(tile)).permute(2, 0, 1).float()
-        normalizer = normalizers.MacenkoNormalizer(backend="torch")
-        normalizer.fit(reference_tensor)
+    def get_normed_tensor(self, idx, resize_to=256):
+        tile = self.get_tile(idx)
+        if resize_to is not None:
+            tile = tile.resize((resize_to, resize_to))
+        return self.normalizer(tile)
 
-
-    def get_tile(self, tile_tuple):
-        patient_path = self.patients[tile_tuple[0]]
+    def get_tile(self, idx):
+        row = self.df[idx]
+        patient_path = self.patient_lookup[row["patient_name"]]
         slide = WSISlide(patient_path)
-        tile_crds = self.tile_lookup[patient_path.name][tile_tuple[1]]
-        tile = slide.read(tile_crds[0], tile_crds[1])
-        return tile
+        crds = row["crd"]
+        return slide.read(crds[0], crds[1])
     
     # def get_svs(self, patient_path):
     #     x = [f for f in patient_path.iterdir() if f.suffix == ".svs"]
     #     return x[0]
     
-    def get_chunk(self, patient_number, x=-1, y=-1, w=1920, h=1080):
-        path = self.patients[patient_number]
+    def get_chunk(self, patient_id, x=-1, y=-1, w=1920, h=1080):
+        if type(patient_id) != str:
+            patient_id = list(self.patient_lookup.keys())[patient_id]
+        path = self.patient_lookup[patient_id]
         slide = WSISlide(path)
         width, height = slide.dim
+
         
         if x == -1 and y == -1:
             x = (width // 2) - (w // 2)
             y = (height // 2) - (h // 2)
         
-        image = slide.read(x, y, w, h)
+        aspect = slide.read_size // TILE_SIZE
+        
+        image = slide.read(x, y, round(aspect * w), round(aspect * h), w, h)
         image.show()
-
-    def check_empty(self, tile):
-        arr = np.array(tile)
-        return np.all(arr == 255)
         
 
 
@@ -133,16 +149,22 @@ class WSISlide:
         self.y = float(self.slide.properties.get(openslide.PROPERTY_NAME_MPP_Y))
         self.mag = float(self.slide.properties.get(openslide.PROPERTY_NAME_OBJECTIVE_POWER))
         self.dim = self.slide.level_dimensions[0]
-    
-    def read(self, x, y, w=TILE_SIZE, h=TILE_SIZE):
-        return self.slide.read_region((x, y), 0, (w, h)).convert("RGB")
+        if ACCOUNT_FOR_MPP:
+            self.read_size = int(TILE_SIZE * (self.x / REFERENCE_MPP))
+        else:
+            self.read_size = TILE_SIZE
 
-    
+    def read(self, x, y, w_in=-1, h_in=-1, w_out=TILE_SIZE, h_out=TILE_SIZE):
+        w_in = self.read_size if w_in == -1 else w_in
+        h_in = self.read_size if h_in == -1 else h_in
+        return self.slide.read_region((x, y), 0, size=(w_in, h_in)).convert("RGB").resize((w_out, h_out))
+
+    def read_raw(self, x, y):
+        return self.slide.read_region((x, y), 0, size=(self.read_size, self.read_size)).convert("RGB")
 
 if __name__ == "__main__":
     l = WSI_loader("test")
-    l.enumerate_tiles()
-    l.get_chunk(2, 0, 0, 10000, 10000)
+    l.get_chunk(0, w=10000, h=10000)
 
 
 
